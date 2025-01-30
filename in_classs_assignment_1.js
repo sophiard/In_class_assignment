06674a3a99ef6eb696d78bdeabeb314f5d6aa1a0
Sophia Dolan <25srdolan@gmail.com>
2025-01-30 16:17:42 -0500

    completed task 2

--- in_classs_assignment_1.js
+++ in_classs_assignment_1.js
@@ -1,1 +1,27 @@
 // Task 1
+
+let x = 10;
+if (x>5) {
+    console.log("help")
+};
+
+let expenses = 10000;
+if (expenses > 7000) {
+    console.log ("High expenses")
+} else {
+    console.log("manageable expenses")
+};
+
+// task 2
+
+let score = 78
+if (score >= 60) {
+    console.log("Passed")
+} else {
+    console.log("Failed")
+}
+
+//task3
+
+
+
